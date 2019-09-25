0443c168b45f9ca29a066cc9d107b5d84c41f51d
Chaitanya Pawa <chaitanya.pawa@mtxb2b.com>
2019-09-25 15:36:53 -0400

    please chal jaa

--- docs/pages/blog/2019-developer-survey-results.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import markdown from './2019-developer-survey-results.md';
-
-export default function Page() {
-  return <MarkdownDocs markdown={markdown} blog disableAd disableToc disableEdit />;
-}
--- docs/pages/blog/july-2019-update.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs';
-import markdown from './july-2019-update.md';
-
-export default function Page() {
-  return <MarkdownDocs markdown={markdown} blog disableAd disableToc disableEdit />;
-}
